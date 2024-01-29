1f51b997ac3fb8ac5d45358394f841b8efade4a4
Adam Nordquist <apnordquist@gmail.com>
2024-01-29 10:32:58 -0700

    Initialization

---
+++ Lab2/Lab2/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Employee
+    {
+        //attributes
+        public string id;
+        public string name;
+        public string address;
+
+        //constructor
+        public Employee(string id, string name, string address)
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+        }
+
+
+
+        //employee
+        //employee(id:string)
+        //address:string
+        //phone: string
+    }
+}
